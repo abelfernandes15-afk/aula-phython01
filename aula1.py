7063b15c59f6fbf5bac8e8c9548ddbaeeab6cd36
Douglas Abel Fernandes <1051556@senacminas.edu.br>
2026-02-26 11:52:27 -0300

    novos arquivos

---
+++ aula1.py
@@ -0,0 +1,5 @@
+#inicio de codico
+produto = 'notebook'
+quantidade = 2
+preço_unitario = 3500.52
+print (f'valo final da compra e $ {3500.52 *2:.1f}')
---
+++ musica.py
@@ -0,0 +1,14 @@
+musica = """
+Lá onde o Sol nascia
+Onde a pipa estancava
+Portão de madeira na segunda casa
+Lazer sem piscina
+House improvisada
+Crescia o moleque no Sol da quebrada
+Que acreditou no que era medo
+Matou a inveja e seus segredos
+Percebeu o gênio do espelho que realiza os seus desejos
+
+
+"""
+print(musica)
