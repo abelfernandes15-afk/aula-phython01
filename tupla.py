c595c2122de98f50846f3b98b476aa8e21e1e2e3
Douglas Abel Fernandes <1051556@senacminas.edu.br>
2026-03-02 08:25:38 -0300

    nova alteraçao

--- tupla.py
+++ tupla.py
@@ -1,3 +1,3 @@
-aluno = ('douglas',38,'aux produçao',2500,00)
+aluno = ('douglas',38 ,2500.00)
 nome, idade, salario = aluno
-print(f"{nome} tem {idade} e pretende receber{salario}".)
+print(f"{nome} tem {idade} e pretende receber{salario}.")
